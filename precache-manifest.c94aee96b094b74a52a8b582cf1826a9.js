ad6cfe25053d8a9ed6a7a68c437cf73cc093d0e7
prasanthi68 <prasanthi.kotagiri8@gmail.com>
2020-06-23 20:26:17 +0530

    Updates

---
+++ precache-manifest.c94aee96b094b74a52a8b582cf1826a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5818dfb0a3ffbea454fe480f2d6285",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "022cca928f54cd171361",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a985db1741ab996abb6",
+    "url": "/react-gh-pages/static/js/2.84e09d9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.84e09d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022cca928f54cd171361",
+    "url": "/react-gh-pages/static/js/main.689d00eb.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  },
+  {
+    "revision": "ac1ae8c497a46b4263f35bb0f60d8fc0",
+    "url": "/react-gh-pages/static/media/pic1.ac1ae8c4.png"
+  }
+]);
